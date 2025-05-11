9ac15fa70897febd7009e426731bb64b1b840162
nareshb07 <k.nareshb0077@gmail.com>
2025-05-11 19:06:43 +0530

    improved UI 0.1

--- promptvault_backend/api/admin.py
+++ promptvault_backend/api/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 from allauth.socialaccount.models import SocialAccount
+from .models import Tag, Prompt
 
 # Override __str__ to force string evaluation
 SocialAccount.__str__ = lambda self: str(super(SocialAccount, self).__str__())
+
+admin.site.register(Tag)
+admin.site.register(Prompt)
